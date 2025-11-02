947b659fcc74b43c3e755b16036484e1f9769eec
StudyGuidesForYou <very.cool.email2001@gmail.com>
2025-11-01 22:29:38 -0400

    Update graphicPresets.js

--- graphicPresets.js
+++ graphicPresets.js
@@ -1,10 +1,10 @@
 export const GraphicsPresets = [
-  { name: "Potato", resolution: 0.5, viewDistance: 300, detail: 16 },
-  { name: "Toaster Mode", resolution: 0.7, viewDistance: 450, detail: 32 },
-  { name: "Normal Human", resolution: 1.0, viewDistance: 650, detail: 48 },
-  { name: "Beast Mode", resolution: 1.25, viewDistance: 900, detail: 64 },
-  { name: "GPU Killer", resolution: 1.5, viewDistance: 1200, detail: 96 },
-  { name: "CPU Destroyer", resolution: 2.0, viewDistance: 2000, detail: 128 }
+  { name: "Potato", resolution: 0.5, viewDistance: 300, terrainDetail: 16 },
+  { name: "Toaster Mode", resolution: 0.7, viewDistance: 450, terrainDetail: 32 },
+  { name: "Normal Human", resolution: 1.0, viewDistance: 650, terrainDetail: 48 },
+  { name: "Beast Mode", resolution: 1.25, viewDistance: 900, terrainDetail: 64 },
+  { name: "GPU Killer", resolution: 1.5, viewDistance: 1200, terrainDetail: 96 },
+  { name: "CPU Destroyer", resolution: 2.0, viewDistance: 2000, terrainDetail: 128 }
 ];
 
 export function getPresetNames() {
