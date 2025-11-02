019c3a575c5cdb8229eecc0af7ce9aa1894de8fa
StudyGuidesForYou <very.cool.email2001@gmail.com>
2025-11-01 22:39:18 -0400

    Update world.js

--- world.js
+++ world.js
@@ -2,49 +2,34 @@ import * as THREE from 'https://unpkg.com/three@0.154.0/build/three.module.js';
 import { applyMode } from './environment.js';
 
 export class World{
-  constructor(scene, opts={}){
-    this.scene=scene;
-    this.opts=opts;
-    this.createTerrain(opts.detail);
-    this.createRoad();
-    applyMode(this.scene, opts.worldMode||'natural', opts);
-  }
-
-  createTerrain(detail=1){
-    const geom=new THREE.PlaneGeometry(2000,2000,64*detail,64*detail);
-    geom.rotateX(-Math.PI/2);
-    for(let i=0;i<geom.attributes.position.count;i++){
-      geom.attributes.position.setY(i,Math.random()*5);
+    constructor(scene, opts={}){
+        this.scene = scene;
+        this.opts = opts;
+        this.createRoad();
+        applyMode(scene, opts.worldMode||'natural', opts);
     }
-    geom.computeVertexNormals();
-    const mat=new THREE.MeshStandardMaterial({color:0x66804b, roughness:1});
-    this.terrain=new THREE.Mesh(geom,mat);
-    this.terrain.receiveShadow=true;
-    this.scene.add(this.terrain);
-  }
 
-  createRoad(){
-    const curve=new THREE.CatmullRomCurve3([
-      new THREE.Vector3(-20,0,0),
-      new THREE.Vector3(0,0,200),
-      new THREE.Vector3(15,0,400),
-      new THREE.Vector3(-10,0,600),
-      new THREE.Vector3(0,0,800)
-    ]);
-    const shape=new THREE.Shape();
-    shape.moveTo(-5,0);
-    shape.lineTo(5,0);
-    const geo=new THREE.ExtrudeGeometry(shape,{steps:200, bevelEnabled:false, extrudePath:curve});
-    const mat=new THREE.MeshStandardMaterial({color:0x333333, roughness:0.8});
-    this.road=new THREE.Mesh(geo,mat);
-    this.road.receiveShadow=true;
-    this.scene.add(this.road);
-  }
+    createRoad(){
+        const curve = new THREE.CatmullRomCurve3([
+            new THREE.Vector3(-20,0,0),
+            new THREE.Vector3(0,0,200),
+            new THREE.Vector3(15,0,400),
+            new THREE.Vector3(-10,0,600),
+            new THREE.Vector3(0,0,800)
+        ]);
+        const shape = new THREE.Shape();
+        shape.moveTo(-5,0);
+        shape.lineTo(5,0);
+        const geo = new THREE.ExtrudeGeometry(shape,{steps:200, bevelEnabled:false, extrudePath:curve});
+        const mat = new THREE.MeshStandardMaterial({color:0x333333, roughness:0.8});
+        this.road = new THREE.Mesh(geo, mat);
+        this.road.receiveShadow = true;
+        this.scene.add(this.road);
+    }
 
-  update(playerZ, dt){
-    if(this.terrain) this.terrain.position.z=playerZ;
-    if(this.road) this.road.position.z=playerZ;
-  }
+    update(carZ, dt){
+        if(this.road) this.road.position.z = carZ;
+    }
 }
 
 window.World=World;
