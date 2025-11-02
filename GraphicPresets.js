c283b687b6372f5c5ea926fe3817be0ed8d887d6
StudyGuidesForYou <very.cool.email2001@gmail.com>
2025-11-01 22:04:07 -0400

    Create GraphicPresets.js

---
+++ GraphicPresets.js
@@ -0,0 +1,65 @@
+// File: graphicsPresets.js
+// Funny graphics preset names mapped to detail + viewDistance + renderScale
+
+export const GRAPHICS_PRESETS = [
+  {
+    name: "Potato",
+    detail: 0.3,
+    viewDistance: 300,
+    renderScale: 0.5,
+  },
+  {
+    name: "Toaster Edition",
+    detail: 0.5,
+    viewDistance: 500,
+    renderScale: 0.75,
+  },
+  {
+    name: "Casual",
+    detail: 0.8,
+    viewDistance: 900,
+    renderScale: 1.0,
+  },
+  {
+    name: "Smooth Gamer",
+    detail: 1.0,
+    viewDistance: 1400,
+    renderScale: 1.2,
+  },
+  {
+    name: "Epic",
+    detail: 1.4,
+    viewDistance: 1800,
+    renderScale: 1.5,
+  },
+  {
+    name: "Ultra",
+    detail: 1.7,
+    viewDistance: 2300,
+    renderScale: 1.7,
+  },
+  {
+    name: "Ludicrous",
+    detail: 2.0,
+    viewDistance: 3000,
+    renderScale: 2.0,
+  },
+  {
+    name: "NASA Computer",
+    detail: 2.4,
+    viewDistance: 3800,
+    renderScale: 2.3,
+  },
+  {
+    name: "MILITARY SIM MODE",
+    detail: 3.0,
+    viewDistance: 5000,
+    renderScale: 2.6,
+  },
+  {
+    name: "CPU DESTROYER",
+    detail: 4.0,
+    viewDistance: 7000,
+    renderScale: 3.0,
+  },
+];
