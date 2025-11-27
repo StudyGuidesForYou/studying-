5931b30087cece1896fd33b9ab745951b2cb034a
StudyGuidesForYou <very.cool.email2001@gmail.com>
2025-11-27 12:08:42 -0500

    Update clock.jsx

--- src/apps/clock.jsx
+++ src/apps/clock.jsx
@@ -2,5 +2,5 @@ import React, { useEffect, useState } from 'react'
 export default function Clock(){
   const [t, setT] = useState(new Date())
   useEffect(()=> { const id = setInterval(()=> setT(new Date()), 1000); return ()=> clearInterval(id) }, [])
-  return <div style={{fontSize:36}}>{t.toLocaleTimeString()}</div>
+  return <div style={{ fontSize:36 }}>{t.toLocaleTimeString()}</div>
 }
