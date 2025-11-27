3ce8f9bf7e87b1c7c77ddcd43b66bd0cc7569d0d
StudyGuidesForYou <very.cool.email2001@gmail.com>
2025-11-27 12:25:11 -0500

    Update Main.jsx

--- src/Main.jsx
+++ src/Main.jsx
@@ -4,7 +4,6 @@ import Apps from './components/Apps.jsx';
 import './styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
   <React.StrictMode>
     <Apps />
