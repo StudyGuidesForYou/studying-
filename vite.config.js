1af266165ed90770abafbf0fc6abbb17cf86166c
StudyGuidesForYou <very.cool.email2001@gmail.com>
2025-11-27 12:33:28 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/studying-/', // matches your GitHub Pages repo name
   plugins: [react()],
+  base: '/studying-/', // must match your repo name for GitHub Pages
 });
