e7688021ccde0897b7e2e979e5167523aeaefc67
StudyGuidesForYou <very.cool.email2001@gmail.com>
2025-11-27 12:34:21 -0500

    Update Taskbar.jsx

--- src/components/Taskbar.jsx
+++ src/components/Taskbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Taskbar({ openApps, onOpen, onClose }) {
   return (
     <div className="taskbar">
-      {openApps.map((appName) => (
+      {openApps.map(appName => (
         <div key={appName} className="taskbar-item">
           <button onClick={() => onOpen(appName)}>{appName}</button>
           <button onClick={() => onClose(appName)}>X</button>
